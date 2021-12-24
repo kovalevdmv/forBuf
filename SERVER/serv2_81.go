ee7c16bdd26a93a9357f3929c998806e456c9bc7
Иван Иванов <kovalevdmv@yandex.ru>
2021-12-24 19:17:10 +0300

    +server

---
+++ SERVER/serv2_81.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("listen port 80")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method == "GET" {
+			key := r.URL.Query().Get("key")
+
+			dat, err := os.ReadFile(key)
+			if err != nil {
+				fmt.Fprintf(w, base64.StdEncoding.EncodeToString([]byte("null")))
+			}
+			fmt.Fprintf(w, base64.StdEncoding.EncodeToString([]byte(string(dat))))
+			log.Println("ok get for key=" + key)
+
+			return
+		}
+
+		body, _ := ioutil.ReadAll(r.Body)
+		str := string(body)
+
+		re, _ := regexp.Compile(`key=.*?,`)
+		res := re.FindAllString(str, -1)
+		key := strings.Replace(res[0], "key=", "", 1)
+		key = strings.Replace(key, ",", "", 1)
+
+		os.WriteFile(key, body, 0644)
+		log.Println(key)
+		log.Println("save file for key=" + key)
+
+		fmt.Fprintf(w, "ok")
+
+	})
+	http.ListenAndServe(":80", nil)
+}
